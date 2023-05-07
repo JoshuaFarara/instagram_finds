665357db2810e675847484df6d88eeb02b16d62b
JoshuaFarara <joshua.farara@gmail.com>
2023-05-07 13:41:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Instagram Finds 
+A collection of instagram coding content that is ran and tested on my own. The purpose of this is to practice small problems, keep up with the community, add my 2cents and remix content. Keep myself sharp with industry standards and implement fun tricks and shortcuts by learning from peers!
+
+Visit github repo to see all of the code testing, duplications, remixing and visit my instagram for all of the fun code implementation and display. 
---
+++ insta_quiz1.py
@@ -0,0 +1,23 @@
+'''
+This quiz is by:
+
+My initial answer is 13. 
+Hypothesis: Since the function is called, the x becomes 12, but the function does not return the value of x, 
+so when it is printed, it uses the initial variable value of 13.
+
+Debugging: Debug the code to see exactly what is happening. 
+
+Answer: fix the mistake and get a proper output.
+
+Customize: Add your own take or feel to the code, to either get the same output, or to get a more enhanced output or some alteration.
+            Explain your process.  
+'''
+
+# What will the answer be?
+x = 13
+def san():
+    x = 12
+
+san(x)
+print (x)
+
